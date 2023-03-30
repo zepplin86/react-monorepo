caf62c84dd79e327f2be45cd5b6b4f6969ed59d7
JaeYoung Park <jy.park@catenoid.net>
2023-03-30 22:35:47 +0900

    jyp commit

--- UI/src/Button.js
+++ UI/src/Button.js
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: #01B798;
+  border:none;
+  font-size: 15px;
+  border-radius: 4px;
+  font-weight: bold;
+  &:hover {
+    opacity: 0.7;
+    color: white;
+  }
+`
 
 export default function PokeButton({ name }) {
     useEffect(() => {
         console.log(name);
     }, []);
     return (
-        <button>
+        <Button>
             {name}
-        </button>
+        </Button >
     );
 }
